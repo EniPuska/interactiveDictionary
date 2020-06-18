cca33788933c2aa296f6ea6eb1c1d3781fe78ca1
EniPuska <eni.puska@gmail.com>
2020-06-18 15:44:28 +0300

    v1

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-print("Hello")
+import json
+from difflib import SequenceMatcher
+
+data = json.load(open("data.json"))
+
+def translate(word):
+    word = word.lower()
+    if word in data:
+        SequenceMatcher(None, )
+        return data[word]
+        '''for i in data[word]:
+            print(i + "\n")'''
+    else:
+        return "The word doesn't exist. Please double check it"
+
+word = input("Enter a word: ")
+
+print(translate(word))
