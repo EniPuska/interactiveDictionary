1a0fb3cac8d5069148d66f8ca9615734fa94497e
EniPuska <eni.puska@gmail.com>
2020-06-19 12:11:48 +0300

    finish app

--- app.py
+++ app.py
@@ -6,14 +6,23 @@ data = json.load(open("data.json"))
 def translate(word):
     word = word.lower()
     if word in data:
-        return data[word]
-        '''for i in data[word]:
-            print(i + "\n")'''
+        return data[word]     
     elif len(get_close_matches(word, data.keys(), cutoff=0.8)) > 0:
-        return "Did you mean %s instead? " % get_close_matches(word, data.keys())[0]
+        yerOrNo = input("Did you mean %s instead? Enter Y if yes or N if no: " % get_close_matches(word, data.keys())[0])
+        if yerOrNo == "Y":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif yerOrNo == "N":
+            return "The word doesn't exist. Please double check it!"
+        else:
+            return "We didnt understand your entry!"
     else:
-        return "The word doesn't exist. Please double check it"
+        return "The word doesn't exist. Please double check it!"
 
+def output(output):
+    if type(output) == list:
+        for i in output:
+            print(i)
+    else:
+        print(output)
 word = input("Enter a word: ")
-
-print(translate(word))
+output(output = translate(word))
